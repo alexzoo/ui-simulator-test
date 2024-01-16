0a0708d5fe1502d776e8607883c44017c0138230
Alexey Zubov <zubov.alexey@gmail.com>
2024-01-16 17:09:42 +0100

    make parametrize tests

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,4 +1,4 @@
-import time
+from pytest import mark
 
 
 def test_successful_login(index_page):
@@ -6,28 +6,41 @@ def test_successful_login(index_page):
     index_page.verify_url()
 
 
-def test_password_incorrect(index_page):
-    index_page.login('correct_username', '123')
-    index_page.verify_error_message('Password or username is incorrect')
+# def test_password_incorrect(index_page):
+#     index_page.login('correct_username', '123')
+#     index_page.verify_error_message('Password or username is incorrect')
+#
+#
+# def test_username_incorrect(index_page):
+#     index_page.login('123', 'correct_password')
+#     index_page.verify_error_message('Password or username is incorrect')
+#
+#
+# def test_empty_password(index_page):
+#     index_page.login('correct_username', '')
+#     index_page.verify_error_message('Password field cannot be empty')
+#
+#
+# def test_empty_username(index_page):
+#     index_page.login('', 'correct_password')
+#     index_page.verify_error_message('Username field cannot be empty')
 
 
-def test_username_incorrect(index_page):
-    index_page.login('123', 'correct_password')
-    index_page.verify_error_message('Password or username is incorrect')
+password_incorrect = ['correct_username', 'password', 'Password or username is incorrect']
+username_incorrect = ['user', 'correct_password', 'Password or username is incorrect']
+empty_password = ['correct_username', '', 'Password field cannot be empty']
+empty_username = ['', 'correct_password', 'Username field cannot be empty']
 
 
-def test_empty_password(index_page):
-    index_page.login('correct_username', '')
-    index_page.verify_error_message('Password field cannot be empty')
-
-
-def test_empty_username(index_page):
-    index_page.login('', 'correct_password')
-    index_page.verify_error_message('Username field cannot be empty')
+@mark.parametrize('username, password, error_message', (password_incorrect, username_incorrect, empty_password, empty_username),
+                  ids=['password_incorrect', 'username_incorrect', 'empty_password', 'empty_username'])
+def test_unsuccessful_login(index_page, username, password, error_message):
+    index_page.login(username=username, password=password)
+    index_page.verify_error_message(error_message)
 
 
 """
-1. сделать через параметризацию
-2. опубликовать в github
-3. добавить в гитигнор файлы которые считаешь нужными 
+1. сделать через параметризацию - done
+2. опубликовать в github - done
+3. добавить в гитигнор файлы которые считаешь нужными  - done
 """
